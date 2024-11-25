412ede11404ea8bdd3de23ebd562513411e7939b
sdubois2 <111801641+sdubois2@users.noreply.github.com>
2024-11-25 10:42:21 -0600

    Update ReaderWriterSem.c

--- src/ReaderWriterSem.c
+++ src/ReaderWriterSem.c
@@ -3,7 +3,45 @@
 #include <pthread.h>
 #include <semaphore.h>
 
+/* This is the code I pulled directly from the book for the problem */
+// typedef struct _rwlock_t { 
+//     sem_t lock; // binary semaphore (basic lock)
+//     sem_t writelock; // allow ONE writer/MANY
+//     readers int readers; // #readers in critical section
+// } rwlock_t; void
+
+// rwlock_init(rwlock_t *rw) {
+//     rw->readers = 0;
+//     sem_init(&rw->lock, 0, 1);
+//     sem_init(&rw->writelock, 0, 1);
+// }
+
+// void rwlock_acquire_readlock(rwlock_t *rw) {
+//     sem_wait(&rw->lock); rw->readers++;
+//     if (rw->readers == 1) // first reader gets writelock
+//     sem_wait(&rw->writelock);
+//     sem_post(&rw->lock);
+// }
+
+// void rwlock_release_readlock(rwlock_t *rw) {
+//     sem_wait(&rw->lock);
+//     rw->readers--;
+//     if (rw->readers == 0){ // last reader lets it go
+//         sem_post(&rw->writelock);
+//     }
+//     sem_post(&rw->lock);
+// }
+
+// void rwlock_acquire_writelock(rwlock_t *rw) {
+//     sem_wait(&rw->writelock);
+// }
+
+// void rwlock_release_writelock(rwlock_t *rw) {
+//     sem_post(&rw->writelock);
+// }
+
 int main(){
+    
     //accepts 10 arguments from the command line denoted as b1, b2, b3, b4, b5, b6, b7, b8, b9, b10
 
     //is either 0 or 1, o is reader, 1 is writer
